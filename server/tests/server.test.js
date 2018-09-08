4425c13bb5fbf773085d92bfcdd4a9b7baace527
Jerry Jones <ag03651@A023941.local>
2018-09-08 11:08:38 -0400

    Setup separate test and development envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,15 +5,16 @@ const {ObjectID} = require('mongodb')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
-const todos = [{
-    _id: new ObjectID(),
-    text: 'First test todo'
-},{
-    _id: new ObjectID(),
-    text: 'Second test todo',
-    completed: true,
-    completedAt: 333
-}
+const todos = [
+    {
+        _id: new ObjectID(),
+        text: 'First test todo'
+    },{
+        _id: new ObjectID(),
+        text: 'Second test todo',
+        completed: true,
+        completedAt: 333
+    }
 ]
 
 beforeEach((done) => {
